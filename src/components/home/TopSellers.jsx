b3947447693a38b3bdea047a6fd98959a4d08309
unbeqem <tristank2005@web.de>
2023-04-20 13:33:54 +0200

    fetching api

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React,  {useState, useEffect} from "react";
+import axios from 'axios'
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 
 const TopSellers = () => {
+  const [topSellers, setTopSellers] = useState([])
+  async function main() {
+    const { data } = await axios.get(
+      'https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers'
+    )
+    setTopSellers(data)
+  }
+
+  useEffect(() => {
+    main()
+  }, [])
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -15,21 +27,21 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
+              {topSellers.map((topSellers, index) => (
                 <li key={index}>
                   <div className="author_list_pp">
-                    <Link to="/author">
+                    <Link to={`/author/${topSellers.authorId}`}>
                       <img
                         className="lazy pp-author"
-                        src={AuthorImage}
+                        src={topSellers.authorImage}
                         alt=""
                       />
                       <i className="fa fa-check"></i>
                     </Link>
                   </div>
                   <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
+                    <Link to={`/author/${topSellers.authorId}`}>{topSellers.authorName}</Link>
+                    <span>{topSellers.price} ETH</span>
                   </div>
                 </li>
               ))}
